7cbe13b08e70bc949fdabdb0ae1068ef9a2059b9
CHCGull <CHC@lxrentdemacbook-pro.local>
2016-07-13 14:43:19 +0800

    Initial Commit

---
+++ HCRealm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCRealm
+//
+//  Created by LucyCui on 16/7/13.
+//  Copyright © 2016年 chc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HCRealm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCRealm
+//
+//  Created by LucyCui on 16/7/13.
+//  Copyright © 2016年 chc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
